4f4a7739d77a04b2b310b75becd4bcb4c03fef0f
Duy Nhất <42767835+duynhatldn@users.noreply.github.com>
2019-01-05 22:12:23 +0700

    Add files via upload

---
+++ chapter09.cpp
@@ -0,0 +1,29 @@
+/*
+ * chapter09.cpp
+ *
+ *  Created on: Jan 2, 2019
+ *      Author: nhat
+ */
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+struct PS
+{
+	int tu;
+	int mau;
+};
+void input(PS &a)
+{
+	cout<<"nhap tu so: "<<endl;
+	cin>>a.tu;
+	cout<<"nhap mau so: "<<endl;
+	cin>>a.mau;
+}
+void output(PS &a)
+{
+	cout<<""
+}
+
