146beb96a35e60fff2dc8deef7cf52226b57c49b
Duy Nhất <42767835+duynhatldn@users.noreply.github.com>
2019-09-14 23:05:43 +0700

    Add files via upload

--- stackduongdi.cpp
+++ stackduongdi.cpp
@@ -1,93 +1,67 @@
 #include <stdio.h>
 #include <iostream>
-#include <stack>
 using namespace std;
-int xet(int a[][10]);
-void duongdi(int a[][10]);
-void showstack(stack <int> s);
+typedef struct typeSTACK
+{
+    int* StkArray;
+    int StkMax;
+    int StkCount;
+}STACK;
+void initstack(STACK& s,int maxitems);
+bool isempty(int a[],int n);
+bool isfull(int a[], int n);
+void push(int a[],int b,int n);
+int pop(int a[], int n);
 int main()
 {
-    int a[][10]=
-    {
-        {0,1,1,0,0},
-        {1,0,1,1,1},
-        {1,1,0,1,1},
-        {0,1,1,0,1},
-        {0,1,1,1,0}
-    };
-    if (xet(a)==1) cout<<"do thi co chu ki"<<endl;
-    else if (xet(a)==2)
-    {
-        cout<<"do thi co duong di"<<endl;
-        duongdi(a);
-    }
-    else if (xet(a)==3) cout<<"do thi binh thuong"<<endl;
+    STACK s;
+    int maxitems;
+    cin>>maxitems;
+    initstack(s,maxitems);
+    
     return 0;
 }
-int xet(int a[][10])
+void initstack(STACK& s,int maxitems);
 {
-    int b[10]={0};
-    int count=0;
-    for(int i=0;i<5;i++)
-    {
-        for(int j=0;j<5;j++)
-        {
-            b[i]=b[i]+a[i][j];
-        }
-    }
-    for(int i=0;i<5;i++)
-    {
-        if (b[i]%2==1) count++;
-        if (count==3) return 3;
-    }
-    if (count==2) return 2;
-    return 1;
+    
 }
-void duongdi(int a[][10])
+
+bool isempty(int a[],int n)
 {
-    int b[10]={0};
-    int k=0;
-    for(int i=0;i<5;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<5;j++)
-        {
-            b[i]=b[i]+a[i][j];
-        }
+        if (a[i]!=NULL) return false;
     }
-    for (int i=0;i<5;i++)
+    return true;
+}
+bool isfull(int a[], int n)
+{
+    if (a[n-1]!=NULL) return true;
+        else return false;
+}
+void push(int a[],int b,int n)
+{
+    for(int i=0;i<n;i++)
     {
-        if (b[i]%2==1)
+        if(a[i]==NULL)
         {
-            k=i;
+            a[i]=b;
             break;
         }
     }
-    stack <int> s;
-    int j=0;
-    s.push(k);
-    while(j<8)
-    {
-        for(int i=0;i<6;i++)
-        {
-            if (a[k][i]==1)
-            {
-                s.push(i);
-                a[k][i]=0;
-                a[i][k]=0;
-                k=i;
-                break;
-            }
-        }
-        j++;
-    }
-    showstack(s);
 }
-void showstack(stack <int> s)
+int pop(int a[],int n)
 {
-    while (!s.empty())
+    int top=a[n-1];
+    for(int i=0;i<n;i++)
     {
-        cout << '\t' << s.top();
-        s.pop();
+        if(a[i]==NULL)
+        {
+            top=a[i-1];
+            a[i-1]=NULL;
+            return top;
+        }
     }
-    cout << '\n';
+    a[n-1]=NULL;
+    return top;
 }
