dd496c0fedfd1a0d84ec73a65c1e19ae0dc692f7
Duy Nhất <42767835+duynhatldn@users.noreply.github.com>
2019-09-27 19:21:34 +0700

    Update stack3.cpp

--- stack3.cpp
+++ stack3.cpp
@@ -1,70 +1,36 @@
 #include <stdio.h>
 #include <iostream>
 using namespace std;
-struct stack
+ 
+int a[101][101]; 
+ 
+int n, m, Free[101], u, v, s;
+ 
+void DFS(int u)
 {
-    int StkArray[20];
-    int StkMax;
-    int StkCount=0;
-}STACK;
-bool isempty(stack s);
-bool isfull(stack s);
-void push(stack &s,int n);
-void pop(stack &s);
-int top(stack s);
-void chuyendoi(stack s,int n);
+    cout << u << endl;
+    Free[u] = false;
+    for (int v = 1; v <= n; v++)
+        if (a[u][v] == 1 && Free[v])
+            DFS(v);
+}
+ 
 int main()
 {
-    struct stack s;
-    cout<<"nhap max cua stack: ";
-    cin>>s.StkMax;
-    cout<<"nhap so: ";
-    int n;
-    cin>>n;
-    chuyendoi(s,n);
+    cin >> n >> m >> s;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            a[i][j] = 0;
+ 
+    for (int i = 1; i <= m; i++)
+    {
+        cin >> u >> v;
+        a[u][v] = 1;
+        a[v][u] = 1;
+    }
+ 
+    for (int i = 1; i <= n; i++)
+        Free[i] = 1;
+    DFS(s);
     return 0;
 }
-bool isempty(stack s)
-{
-    if (s.StkCount==0) return false;
-        return true;
-}
-bool isfull(stack s)
-{
-    if (s.StkArray[s.StkMax]==NULL) return false;
-    return true;
-}
-void push(stack &s,int n)
-{
-    s.StkArray[s.StkCount]=n;
-    s.StkCount++;
-}
-void pop(stack &s)
-{
-    s.StkArray[s.StkCount]=NULL;
-    s.StkCount--;
-}
-int top(stack s)
-{
-    return s.StkArray[s.StkCount];
-}
-void chuyendoi(stack s,int n)
-{
-    int k=n/2;
-    while(k!=0)
-    {
-        push(s,n-k*2);
-        n=k;
-        k=n/2;
-    }
-    push(s,1);
-    pop(s);
-    int h=s.StkCount;
-    for(int i=0;i<h+1;i++)
-    {
-        cout<<top(s);
-        pop(s);
-    }
-    cout<<endl;
-}
-
