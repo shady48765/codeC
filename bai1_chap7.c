b05c74f69f740c5e43994d24cd908d005cb8a613
Duy Nhất <42767835+duynhatldn@users.noreply.github.com>
2019-10-18 21:30:11 +0700

    Update bai1_chap7.c

--- bai1_chap7.c
+++ bai1_chap7.c
@@ -1,50 +1,35 @@
-/*
- * bai1_chap7.c
- *
- *  Created on: Dec 7, 2018
- *      Author: nhat
- */
-
-int arrMaxMin(a[][], int row, int colom)
+void Insert_List (ElementType X,Position p, DoubleList *DL)
 {
-int max=0;
-int min=0;
 
-	for (int i=0;i <row;i++)
+if (*DL == NULL)
 	{
-		for (int j=0;j< colom;j++)
-		{
-			if (a[i][j] <min) min=a[i][j];
-			if (a[i][j] >min) max=a[i][j];
-		}
-	}
-	return 0;
-}
 
-void sosanh(a[][], int row, int colum)
-{
-	int sumxx=0;
-	int sum=0;
+		(*DL)=(Node*)malloc(sizeof(Node));
 
-	if (row==colum) printf("hang bang cot");
-	{
-	for (int i=0;i <row;i++)
-		{
-		sumxx+= a[i][i];
-		sum+= a[i][row-i];
-		}
-	}
-	if (sumxx>sum) printf("cheo xuong lon hon cheo len");
-	else printf("cheo len lon hon cheo xuong");
-}
-void sosanh2(a[][], int row, int colum,b[])
-{
-	for (int i=0;i <row;i++)
-		{
-			for (int j=0;j <colum;j++)
-			{
-				b[i]+=a[i][j];
-			}
-		}
-}
+		(*DL)->Element = X;
 
+		(*DL)->Previous =NULL;
+
+		(*DL)->Next =NULL;
+
+	} else{
+
+		Position temp;
+
+		temp=(Node*)malloc(sizeof(Node));
+
+		temp->Element=X;
+
+		temp->Next=p;
+
+		temp->Previous=p->Previous;
+
+		if (p->Previous!=NULL)
+
+			p->Previous->Next=temp;
+
+		p->Previous=temp;
+
+		}
+
+}
