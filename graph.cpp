a753c25c9afba589546d51823ba6584b2e1af9da
Duy Nhất <42767835+duynhatldn@users.noreply.github.com>
2019-11-12 20:11:56 +0700

    Add files via upload

--- graph.cpp
+++ graph.cpp
@@ -1,138 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
-#include <cmath>
-#include <string>
-#include <vector>
-#include <queue>
-#define UNVISITED -1
-#define VISITED 1
-
 using namespace std;
-
-typedef vector<long> vl;
-typedef vector<int> vi;
-typedef vector<long long> vll;
-
-int N, M;
-int dfsNumberCounter, numSCC;
-vector<vl> graph;
-vi dfs_num, dfs_low, S, visited, contaminated;
-vll routes;
-
-void travel_graph(int start){
-  int u; // current vertex
-  queue<int> q;
-  q.push(start);
-  while(q.empty() == false){
-    u=q.front();
-    // cout << u+1 << " travel current" << endl;
-    visited[u]=1;
-    q.pop();
-    for(int i = 0; i < graph[u].size(); i++){
-      int v = graph[u][i];
-      if(contaminated[v]!=1 && visited[v]!=1){
-        // cout << "DFS at " << u+1 << " on " << v+1 << " with " << routes[v] << " routes " << endl;
-        q.push(v);
-        routes[v] += routes[u];
-        // cout << "Vertex " << u+1 <<" adds to " << v + 1 << endl;
-      }
-    }
-  }
+#define MAX 10
+#define inputfile "C:/test.txt"
+typedef struct GRAPH {
+	int n;
+	int a[MAX][MAX];
+};
+int DocMT(char tenFile[100], DOTHI &g);
+void XuatMT(DOTHI g);
+void kiemtraMT(DOTHI g);
+int main()
+{
+	DOTHI g;
+	DocMT(inputfile,g);
+	XuatMT(g);
+	if(kiemtraMT==0) printf("co huong");
+	return 0;
 }
-
-void tarjanSCC(int u){
-  // cout << u+1 << " current" << endl;
-  dfs_low[u] = dfs_num[u] = dfsNumberCounter++;
-  S.push_back(u);
-  visited[u] = 1;
-  for(int j = 0; j < graph[u].size(); j++){
-    int v = graph[u][j];
-    if(dfs_num[v] == UNVISITED){
-      // cout << "DFS at " << u+1 << " adding " << v+1 << " to stack " << endl;
-
-      tarjanSCC(v);
-    }
-    if(visited[v])
-      dfs_low[u] = min(dfs_low[u], dfs_low[v]);
-  }
+int DocMT(char tenFile[100], DOTHI &g)
+{
+	FILE* f;
+	f=fopen(tebFile,"rt");
+	if(f==NULL)
+	{
+		printf("ko mo dc file\n");
+		return 0;
+	}
+	fscanf(f,"%d",&g.n);
+	for(int i=0;j<g.n;i++)
+	{
+		for(int j=0;j<g.n;j++)
+		{
+			fscanf(f,"%d",&g.a[i][j]);
+		}
+	}
+	fclose(f);
+	return 1;
 }
-
-void print_last_nine(long long w);
-void print_graph();
-
-int main ()
-{ 
-  cin >> N >> M;
-
-  graph.assign(N, vl());
-  dfs_num.assign(N, UNVISITED);
-  dfs_low.assign(N, 0);
-  visited.assign(N, 0);
-  contaminated.assign(N, 0);
-  routes.assign(N, 0);
-  routes[0]=1;
-  dfsNumberCounter = numSCC = 0;
-
-  for(int i = 0; i < M; i++){
-    int a, b;
-    cin >> a >> b;
-    graph[a-1].push_back(b-1);
-  }
-  tarjanSCC(0);
-
-  for(int i = 0; i < S.size(); i++){
-    int vertex = S[i];
-    if(contaminated[vertex]){
-      // cout << vertex+1 << " contaminated c." << endl;
-      for(int j = 0; j < graph[vertex].size();j++){
-        contaminated[graph[vertex][j]]=1;
-      }
-    } else if((dfs_low[vertex] == dfs_low[vertex+1] || dfs_low[vertex] == dfs_low[vertex-1]) && i != 0 ){
-      // cout << vertex+1 << " contaminated." << endl;
-      for(int j = 0; j < graph[vertex].size();j++){
-        contaminated[graph[vertex][j]]=1;
-      }
-      contaminated[vertex]=1;
-    }
-  }
-
-  visited.assign(N, 0);
-  travel_graph(0);
-  if(contaminated[1]){
-    cout << "inf" << endl;
-  } else if(routes[1]>999999999){
-    print_last_nine(routes[1]);
-  } else{
-    cout << routes[1] << endl;
-  }
-
-
-  return 0;
+void XuatMT(DOTHI g)
+{
+	printf("so dinh cua do thi: %d\n",g.n);
+	printf("ma tran ke cua do thi: \n");
+	for(int i=0;i<g.n;i++)
+	{
+		for(int j=0;j<g.n;j++)
+		{
+			printf("%d",g.a[i][j]);
+		}
+		printf("\n");
+		
+	}
 }
-void print_last_nine(long long w){
-  int length = 1;
-  long long x=w%1000000000;
-  while ( x /= 10 )
-    length++;
-  for(int i = 0; i < 9-length; i++){
-    cout << 0;
-  }
-  cout << w%1000000000 << endl;//missing check
-}
-
-void print_graph(){
-  for(int i = 0; i < N; i++){
-    // if(processed[i])
-    //   cout << "P  ";
-    // else
-    //   cout << "nP ";
-    // if(contaminated[i])
-    //   cout << "C  ";
-    // else
-    //   cout << "nC ";
-    cout << i+1 << "<" << routes[i] << ">" << ": ";
-    for(int j=0; j < graph[i].size(); j++ ){
-      cout << graph[i][j]+1 << " ";
-    }
-    cout << endl;
-  }
+void kiemtraMT(DOTHI g)
+{
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(a[i][j]!=a[j][i]) return 0;
+		}
+	}
+	return 1;
 }
