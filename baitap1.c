f7dd6e7384c3683b4174487698d126c2277a59f9
Duy Nhất <42767835+duynhatldn@users.noreply.github.com>
2019-11-25 20:20:32 +0700

    Update baitap1.c

--- baitap1.c
+++ baitap1.c
@@ -1,23 +1,150 @@
-/*
- * baitap1.c
- *
- *  Created on: Nov 8, 2018
- *      Author: nhat
- */
-#include<conio.h>
-#include<stdio.h>
-#include<math.h>
- int main()
- {
-	 int n;
-	 int a=0;
-	 printf("nhap so n:");
-	 scanf("%d",n);
-	 for (int i=0;i<n;i++)
-	 {
-		 a=a + i;
-	 };
-	 printf("ket qua tinh tong = %d",a);
-	 return 0;
- }
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+typedef struct STACK
+{
+	int a[1000];
+	int count;
+}stack;
+typedef struct TOADO
+{
+	int hang;
+	int cot;
+}toado;
+int duyet(char a[][],stack ketqua, toado chay,toado dich,bool traloi)
+{
+	if(chay.cot==0 || chay.hang==0 || chay.cot=m-1 || chay.hang==n-1)
+	{
+		if(dich.cot!=0 && dich.hang!=0 && dich.cot!=m-1 && dich.hang!=n-1)
+		{
+			traloi=false;
+			return 0;
+		}
+		if(chay.cot==0 && dich.cot==0)
+		{
+			if(chay.hang>dich.hang)
+			{
+				int i=dich.hang;
+				int iden=chay.hang;
+			} else
+			{
+				int iden=dich.hang;
+				int i=chay.hang;
+			}
+			int dem;
+			for(i;i<iden;i++)
+			{
+				if(a[i][0]=='#')
+				{
+					dem++;
+					break;
+				}
+			}
+			if(dem==0)
+			{
+				traloi==true;
+				return 1;
+			}else
+			{
+				traloi=false;
+				return 0;
+			}
+		}
+		if(chay.cot==m-1 && dich.cot==m-1)
+		{
+			if(chay.hang>dich.hang)
+			{
+				int i=dich.hang;
+				int iden=chay.hang;
+			} else
+			{
+				int iden=dich.hang;
+				int i=chay.hang;
+			}
+			int dem;
+			for(i;i<iden;i++)
+			{
+				if(a[i][m-1]=='#')
+				{
+					dem++;
+					break;
+				}
+			}
+			if(dem==0)
+			{
+				traloi==true;
+				return 1;
+			}else
+			{
+				traloi=false;
+				return 0;
+			}
+		}
+		if(chay.hang==0 && dich.hang==0)
+		{
+			if(chay.cot>dich.cot)
+			{
+				int i=dich.cot;
+				int iden=chay.cot;
+			} else
+			{
+				int iden=dich.cot;
+				int i=chay.cot;
+			}
+			int dem;
+			for(i;i<iden;i++)
+			{
+				if(a[0][i]=='#')
+				{
+					dem++;
+					break;
+				}
+			}
+			if(dem==0)
+			{
+				traloi==true;
+				return 1;
+			}else
+			{
+				traloi=false;
+				return 0;
+			}
+		}
+		if(chay.hang==n-1 && dich.hang==n-1)
+		{
+			if(chay.cot>dich.cot)
+			{
+				int i=dich.cot;
+				int iden=chay.cot;
+			} else
+			{
+				int iden=dich.cot;
+				int i=chay.cot;
+			}
+			int dem;
+			for(i;i<iden;i++)
+			{
+				if(a[0][n-1]=='#')
+				{
+					dem++;
+					break;
+				}
+			}
+			if(dem==0)
+			{
+				traloi==true;
+				return 1;
+			}else
+			{
+				traloi=false;
+				return 0;
+			}
+		}
+	}
+	if(dich.cot==chay.cot && dich.hang=chay.hang)
+	{
+		traloi==true;
+		return 1;
+	}
+}
